088673e978028752db45ee41fce3e98e6e0ede3a
Mark Viti <+>
2022-06-06 11:03:56 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,16 @@ def reload
   load 'config/environment.rb'
 end
 
+# a = CarOwner.new("Adam")
+# b = CarOwner.new("Betty")
+
+# c = Mechanic.new("Chris", "exotic")
+# d = Mechanic.new("Denis", "junker")
+
+# car1 = Car.new("Honda", "Civic", "junker", d, a)
+# car2 = Car.new("Dodge", "Charger", "muscle", c, a)
+# car3 = Car.new("Porsche", "911", "exoitc", c, b)
+
+
+
 binding.pry
